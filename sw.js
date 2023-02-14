a4231cd06d131a79809dafaa4c2d183734684c45
chronosirius <73508925+chronosirius@users.noreply.github.com>
2023-02-14 15:38:06 -0800

    e

--- sw.js
+++ sw.js
@@ -3,8 +3,33 @@
 /// <reference lib="webworker" /> 
 /**@type {ServiceWorkerGlobalScope} self */
 
-async function stepFetch_fastest({ req, preload}) {
+async function stepFetch_fastest({ request, preload, fallback_cached }) {
+    cache_response = await self.caches.match(request)
+    if (cache_response) {
+        return cache_response;
+    }
 
+    preload_res = await preload;
+    if (preload_res) {
+        return preload_res;
+    }
+
+    try {
+        network_res = await fetch(request)
+        cache = await self.caches.open('main_cache')
+        cache.addAll(network_res.clone())
+        return network_res;
+    } catch (err) {
+        fallback = await self.caches.match(fallback_cached)
+        if (fallback) {
+            return fallback;
+        } else {
+            return new Response('Network died!', {
+                status: 408,
+                headers: {"Content-Type": "text/plain"}
+            })
+        }
+    }
 }
 
 self.addEventListener('install', ev => {
@@ -28,6 +53,8 @@ self.addEventListener('activate', ev => {
 
 self.addEventListener('fetch', ev => {
     ev.respondWith(stepFetch_fastest({
-
+        request: ev.request,
+        preload: ev.preloadResponse,
+        fallback_cached: "offline.html"
     }))
 })
